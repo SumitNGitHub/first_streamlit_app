154a842467b6c3ee5eaf5e6db6f1e0e9c53b98bc
Sumit Nanche <116055032+SumitNGitHub@users.noreply.github.com>
2022-10-18 11:06:39 +0530

    Update Streamlit_app.py
    Added 3rd Breakfast

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -5,4 +5,5 @@ streamlit.title('My parents new healthy Dinner')
 streamlit.header('breakfast menu')
 
 streamlit.text('Omega3 & Blueberry Oatmeal')
-streamit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
