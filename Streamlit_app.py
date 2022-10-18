968a0ce8774f98de7de48b54b22a517a666ddd05
Sumit Nanche <116055032+SumitNGitHub@users.noreply.github.com>
2022-10-18 11:11:03 +0530

    Update Streamlit_app.py
    Added Emoji and 4th Breakfast

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,8 +2,9 @@
 import streamlit
 streamlit.title('My parents new healthy Dinner')
 
-streamlit.header('breakfast menu')
+streamlit.header('Breakfast Favorites')
 
-streamlit.text('Omega3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
