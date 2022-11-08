4f122864eccaa2c02f669fadd033233f33d6817d
Sumit Nanche <116055032+SumitNGitHub@users.noreply.github.com>
2022-11-08 22:23:48 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -31,3 +31,7 @@ my_cur.execute("SELECT * FROM fruit_load_list;")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.multiselect("What fruit would you like to add?:", list(my_data_rows.index))
+
+streamlit.text("Thanks for adding ",add_my_fruit)
