6b79a337e9b342becfff8064d14a48a532cb3728
Sumit Nanche <116055032+SumitNGitHub@users.noreply.github.com>
2022-11-08 19:19:39 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -24,9 +24,9 @@ streamlit.dataframe(fruits_to_show)
 
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list;")
-my_data_row = my_cur.fetchone()
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT * FROM fruit_load_list;")
+#my_data_row = my_cur.fetchone()
 #streamlit.text("The Fruit load list contains:")
-streamlit.text(my_data_row)
+#streamlit.text(my_data_row)
