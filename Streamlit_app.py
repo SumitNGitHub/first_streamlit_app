46d6fe84ab39bf1bd5e09e94526ddd402fd0950f
Sumit Nanche <116055032+SumitNGitHub@users.noreply.github.com>
2022-11-15 17:57:34 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -48,7 +48,7 @@ streamlit.stop()
 
 streamlit.text("The Fruit load list contains:")
 #snowflake realted function
-deg get_fruit_load_list():
+def get_fruit_load_list():
       with my_cnx.cursor() as my_cur:
       my_cur.execute("SELECT * FROM fruit_load_list;")
       return my_cur.fetchall()
